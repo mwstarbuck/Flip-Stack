68ad5f3b66e3f8ec3efa71649e5cb19855472d40
mwstarbuck <mwstarbuck@gmail.com>
2021-12-08 12:57:05 -0600

    card flip with hover transition

--- src/App.js
+++ src/App.js
@@ -7,22 +7,28 @@ import ReactCardFlip from 'react-card-flip';
 
 function App() {
   const [isFlipped, setIsFlipped] = React.useState(false);
+  const [mouseOn, setMouseOn] = React.useState(false);
 
   const handleFlip = () => {
     setIsFlipped(!isFlipped);
   }
+
   return (
-    <div className="App" style={{ backgroundColor: 'black' }} >
-      <ReactCardFlip isFlipped={isFlipped} flipDirection={"horizontal"}>
+    <div className="App" style={{ backgroundColor: 'black', height: 'auto' }} >
+      <ReactCardFlip isFlipped={isFlipped} flipDirection={"horizontal"} >
         <div
           onClick={handleFlip}
+          onMouseOver={() => setMouseOn(true)}
+          onMouseLeave={() => setMouseOn(false)}
         >
-          <img style={{ width: 160 + 'px', margin: 10 + 'px', borderRadius: 10 + 'px' }} src={cardBack} />
+          <img style={{ width: 160 + 'px', margin: 50 + 'px', borderRadius: 12 + 'px' }} src={cardBack} />
         </div>
         <div
           onClick={handleFlip}
+          onMouseOver={() => setMouseOn(true)}
+          onMouseLeave={() => setMouseOn(false)}
         >
-          <img style={{ width: 160 + 'px', margin: 10 + 'px', borderRadius: 10 + 'px' }} src={cardFront} />
+          <img style={{ width: 160 + 'px', margin: 50 + 'px', borderRadius: 12 + 'px', transition: 'all .3s ease-in-out', transform: `${mouseOn && isFlipped ? 'scale(1.3,1.3)' : 'scale(1.0,1.0)'}` }} src={cardFront} />
         </div>
       </ReactCardFlip>
 
