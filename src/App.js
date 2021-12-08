dee056d4de15d5ebc1b6b9c9ab25ebe103673014
mwstarbuck <mwstarbuck@gmail.com>
2021-12-07 18:50:16 -0600

    Card flip functioning

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import cardBack from './img/rouleur_back.png';
+import cardFront from './img/black_r_4.png';
+import ReactCardFlip from 'react-card-flip';
 
 function App() {
+  const [isFlipped, setIsFlipped] = React.useState(false);
+
+  const handleFlip = () => {
+    setIsFlipped(!isFlipped);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className="App" style={{ backgroundColor: 'black' }} >
+      <ReactCardFlip isFlipped={isFlipped} flipDirection={"horizontal"}>
+        <div
+          onClick={handleFlip}
+        >
+          <img style={{ width: 160 + 'px', margin: 10 + 'px', borderRadius: 10 + 'px' }} src={cardBack} />
+        </div>
+        <div
+          onClick={handleFlip}
+        >
+          <img style={{ width: 160 + 'px', margin: 10 + 'px', borderRadius: 10 + 'px' }} src={cardFront} />
+        </div>
+      </ReactCardFlip>
+
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +39,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
